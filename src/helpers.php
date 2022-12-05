1cc5105b5a11bf8d327bc5c0ec82da663b1969b0
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2022-12-05 16:13:32 +0100

    Remove exceptions from helper functions for uploads and images paths

--- src/helpers.php
+++ src/helpers.php
@@ -5,16 +5,10 @@ if (! function_exists('uploads_path')) {
      * Get the path, where the uploaded files are stored
      *
      * @return string
-     * @throws Exception
      */
     function uploads_path(): string
     {
-        $configuredPath = config('networkcontroller.uploads.path');
-        if (empty($configuredPath)) {
-            throw new Exception('To use the uploads controller, set UPLOADS_PATH configuration variable in the .env file to a non-empty string!');
-        }
-
-        return base_path() . '/../' . $configuredPath;
+        return base_path() . '/../' . config('networkcontroller.uploads.path');
     }
 }
 
@@ -23,15 +17,9 @@ if (! function_exists('uploaded_images_path')) {
      * Get the path, where the uploaded images are stored
      *
      * @return string
-     * @throws Exception
      */
     function uploaded_images_path(): string
     {
-        $configuredPath = config('networkcontroller.images.path');
-        if (empty($configuredPath)) {
-            throw new Exception('To use the uploads controller, set IMAGES_PATH configuration variable in the .env file to a non-empty string!');
-        }
-
-        return base_path() . '/../' . $configuredPath;
+        return base_path() . '/../' . config('networkcontroller.images.path');
     }
 }
